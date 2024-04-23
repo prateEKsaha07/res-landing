0ae277eab299450bc8a0d0b143ea5112541ba483
prateEKsaha07 <155552003+prateEKsaha07@users.noreply.github.com>
2024-04-23 10:24:30 +0100

    nothing

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './Navbar';
+
+const Home = () => {
+  return ( <div className="home-container">
+        <Navbar />
+    </div>
+  );
+};
+
+export default Home
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from "../Assets/Logo.svg";
+
+const Navbar = () => {
+  return (
+    <div>
+        <img src={Logo} alt="" />
+    </div>
+  )
+}
+
+export default Navbar 
